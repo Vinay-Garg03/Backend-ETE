66bd93a3b75b1c41b124504bdfb3652b4e913c80
Vinay Garg <vinaygarg03062000@gmail.com>
2021-04-29 16:20:43 +0530

    Updated for heroku

--- vinayEndTermProject/src/main/java/com.exam.vinayEndTermProject/controller/HomeController.java
+++ vinayEndTermProject/src/main/java/com.exam.vinayEndTermProject/controller/HomeController.java
@@ -3,12 +3,17 @@ package com.exam.vinayEndTermProject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/firstPage.html")
+    @RequestMapping("/")
     public String getAllDetails(Model model) {
+        return "firstPage";
+    }
+    @RequestMapping("/firstPage.html")
+    public String getAll(Model model) {
         return "firstPage";
     }
     @RequestMapping("/secondPage.html")
