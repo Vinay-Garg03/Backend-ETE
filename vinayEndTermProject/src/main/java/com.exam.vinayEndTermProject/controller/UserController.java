729eb63acf4e5d43ad42c47c1d8eb8142334f21e
Vinay Garg <vinaygarg03062000@gmail.com>
2021-04-29 16:22:28 +0530

    Updated

--- vinayEndTermProject/src/main/java/com.exam.vinayEndTermProject/controller/UserController.java
+++ vinayEndTermProject/src/main/java/com.exam.vinayEndTermProject/controller/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class UserController {
